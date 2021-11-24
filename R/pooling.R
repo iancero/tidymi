6f64736b5ef14eaf780797c906978f2a36471677
Ian Cero <ianjcero@gmail.com>
2021-11-24 18:04:01 -0500

    Added assertions for basic pooling functions

--- R/pooling.R
+++ R/pooling.R
@@ -1,21 +1,31 @@
 pool_point_est <- function(point_ests){
+  assertthat::assert_that(length(point_ests) > 1)
+  assertthat::noNA(point_ests)
+
   mean(point_ests)
 }
 
 within_imp_var <- function(std_errors){
+  assertthat::assert_that(length(std_errors) > 1)
+  assertthat::noNA(std_errors)
+
   mean(std_errors^2)
 }
 
 between_imp_var <- function(point_ests){
+  assertthat::assert_that(length(point_ests) > 1)
+  assertthat::noNA(point_ests)
+
   var(point_ests)
 }
 
 total_imp_var <- function(point_ests, std_errors){
 
-  stopifnot(
-    length(point_ests) != length(std_errors),
-    length(point_ests) > 1,
-    length(std_errors) > 1)
+  assertthat::is.count(length(point_ests) != length(std_errors))
+  assertthat::assert_that(length(point_ests) > 1)
+  assertthat::assert_that(length(std_errors) > 1)
+  assertthat::noNA(point_ests)
+  assertthat::noNA(std_errors)
 
   var_within <- within_imp_var(std_errors)
   var_between <- between_imp_var(point_ests)
@@ -25,5 +35,11 @@ total_imp_var <- function(point_ests, std_errors){
 }
 
 pool_std_err <- function(point_ests, std_errors){
+  assertthat::is.count(length(point_ests) != length(std_errors))
+  assertthat::assert_that(length(point_ests) > 1)
+  assertthat::assert_that(length(std_errors) > 1)
+  assertthat::noNA(point_ests)
+  assertthat::noNA(std_errors)
+
   sqrt(total_imp_var(point_ests, std_errors))
 }
